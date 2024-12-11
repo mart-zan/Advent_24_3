39662930727638ef36778be61b9bbdc4bedab269
Žaneta Martinů <zaneta.martinu@vdimaging.com>
2024-12-11 15:20:10 +0100

    Task 2

--- main.py
+++ main.py
@@ -12,20 +12,52 @@ def read_instructions(filename: str):
     return data
 
 
-
 if __name__ == '__main__':
     # Read and close file
     instructions = read_instructions("input.txt")
 
     # Regular expression to extract two numbers
-    pattern = r"mul\((\d+),(\d+)\)"
-    numbers_list = re.findall(pattern, instructions)
+    pattern_mul = r"mul\((\d+),(\d+)\)"
+    numbers_list = re.findall(pattern_mul, instructions)
 
+    # Initialization
     sum_mul = 0
 
-    # Inititalization
+    ### TASK 1
     for numbers in numbers_list:
         num1, num2 = numbers
         sum_mul += int(num1) * int(num2)
 
     print("Final sum is " + str(sum_mul))
+
+    ### TASK 2
+    # Patterns
+    # pattern_do = r"\\do()"
+    # pattern_dont = r"\\don't()"
+
+    # Find all instructions do/dont/mul
+    all_matches = re.finditer(r"mul\(\d+,\d+\)|do\(\)|don't\(\)", instructions)
+
+    # Initialization
+    mul_enabled = True  # Multiplications start enabled
+    sum_mul = 0         # Sum of enabled multiplications
+
+    # Process each instruction in order
+    for match in all_matches:
+        instruction = match.group()
+
+        if instruction == "do()":  # If do found
+            mul_enabled = True  # Enable multiplications
+        elif instruction == "don't()":  # If dont found
+            mul_enabled = False  # Disable multiplications
+        else:  # It's a mul(x, y) instruction
+            if mul_enabled:
+                # Extract the two numbers
+                num1, num2 = map(int, re.findall(r"\d+", instruction))
+                sum_mul += num1 * num2
+
+    print("Final sum with do and dont is " + str(sum_mul))
+
+    # numbers_idx = re.finditer(pattern_mul, instructions)
+    # do_idx = re.finditer(pattern_do, instructions)
+    # dont_idx = re.finditer(pattern_dont, instructions)
