9b9af443d5a711985236c838f9af80b17164aad8
Žaneta Martinů <zaneta.martinu@vdimaging.com>
2024-12-11 10:35:29 +0100

    REgular expression pattern.

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+import numpy as np
+import re
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Advent of code
+# --- Day 2: Red-Nosed Reports ---
+def read_instructions(filename: str):
+
+    my_file = open(filename, "r")
+    data = my_file.read()
+    my_file.close()
+    # Returns
+    return data
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Read and close file
+    instructions = read_instructions("input.txt")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Regular expression to extract two nubers
+    pattern = r"mul\((\d+),(\d+)\)"
+    matches = re.findall(pattern, instructions)
+
+
